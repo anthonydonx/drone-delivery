aa1adccbda54477c6dcfe6ea6f5ad89164bf99d0
Asanka Anthony <anthonydonx@gmail.com>
2022-09-18 23:29:35 +0530

    first commit

---
+++ src/main/java/com/musala/dronedelivery/DroneDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.musala.dronedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneDeliveryApplication.class, args);
+	}
+
+}
